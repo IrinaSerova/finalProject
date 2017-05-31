0e95ad5d02457926b6e8dbe70abe29843094a957
Irina Serova <isserova@gmail.com>
2017-05-31 16:58:20 -0700

    added js components: accordion, tabs

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,76 @@
-$('.counter').each(function () {
-    var $this = $(this),
-        countTo = $this.attr('data-count');
+jQuery(document).ready(function ($) {
 
-    $({
-        countNum: $this.text()
-    }).animate({
-            countNum: countTo
-        },
+    // counter
+    $('.counter').each(function () {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
 
-        {
-
-            duration: 8000,
-            easing: 'linear',
-            step: function () {
-                $this.text(Math.floor(this.countNum));
+        $({
+            countNum: $this.text()
+        }).animate({
+                countNum: countTo
             },
-            complete: function () {
-                $this.text(this.countNum);
-                //alert('finished');
-            }
 
-        });
+            {
+
+                duration: 8000,
+                easing: 'linear',
+                step: function () {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+
+            });
 
 
 
+    });
+
+    // end counter
+    // tabs
+
+
+
+    var contentDiv = '<div id="content"></div>';
+    $(contentDiv).appendTo('dl.tabs');
+    $('dl.tabs dd').hide();
+    var content = $('dl.tabs dt.active').next('dd').html();
+    $('#content').html(content);
+
+    $('dl.tabs dt').on('click', function () {
+        $('dl.tabs dt').removeClass('active');
+        content = $(this).addClass('active').next('dd').html();
+        $('#content').html(content);
+    });
+
+
+
+    // end tabs
+    // accordion
+
+
+
+
+    var accItem = document.getElementsByClassName('accordionItem');
+    var accHD = document.getElementsByClassName('accordionItemHeading');
+    for (i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener('click', toggleItem, false);
+    }
+
+    function toggleItem() {
+        var itemClass = this.parentNode.className;
+        for (i = 0; i < accItem.length; i++) {
+            accItem[i].className = 'accordionItem close';
+        }
+        if (itemClass == 'accordionItem close') {
+            this.parentNode.className = 'accordionItem open';
+        }
+    }
+
+
+
+    // end accordion
 });
